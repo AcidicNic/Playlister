c4a7fb71022f284c253db03305ec298182ab43f7
snekek <nic4096@gmail.com>
2019-09-26 11:21:32 -0700

    a few pages in

--- app.py
+++ app.py
@@ -1,11 +1,77 @@
-from flask import Flask, render_template
+from bson import ObjectId
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
+
+# @app.route('/')
+# def index():
+#     """Show all playlists."""
+#     return render_template('home.html', msg=":) :) :)")
+
+
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+]
+
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists.find())
+
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
